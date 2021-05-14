7e224f758b93bc369d4c814d6725f4ea1ccd8167
kuldeep.panwar007 <kuldeeppanwar7697@gmail.com>
2021-05-14 22:22:23 +0530

    final

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
-    document.getElementById("table-div").style.marginLeft = "200px";
+    document.getElementById("main").style.marginLeft = "100px";
+    // document.getElementById("table-div").style.marginLeft = "200px";
     document.getElementById("openbtn").style.visibility="hidden";
     document.getElementById("closebtn").style.visibility="visible";
-    document.getElementById("welcome").style.marginLeft = "200px"; 
+    // document.getElementById("welcome").style.marginLeft = "200px"; 
     
     
 }
@@ -12,8 +12,8 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("table-div").style.marginLeft = "0";
+    // document.getElementById("table-div").style.marginLeft = "0";
     document.getElementById("openbtn").style.visibility="visible";
     document.getElementById("closebtn").style.visibility="hidden";
-    document.getElementById("welcome").style.marginLeft = "0"; 
+    // document.getElementById("welcome").style.marginLeft = "0"; 
 }
